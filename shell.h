ab5703a87a514d57e1d6a9a1ed6568e3617c5b10
niyo-sean <niyibizibonaventure33@gmail.com>
2023-09-20 09:40:20 +0200

    1_tast for correction

--- shell.h
+++ shell.h
@@ -13,13 +13,7 @@
 
 void niyo_play(const char *message, const char *color);
 void dis_prompt(void);
-void env_to_path(const char *path);
 void read_command(char *command, size_t size);
 void exec_command(const char *command);
-
-
-
-
-
 
 #endif /*My shell header  Done by niyo-sean */
